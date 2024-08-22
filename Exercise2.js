e1f0021e82baba172a157ac4db60a6648daca914
tuyishimehono <tuhonori1@gmail.com>
2024-08-22 16:46:39 +0200

    Add function to check for prime numbers

--- Exercise2.js
+++ Exercise2.js
@@ -1,1 +1,8 @@
 // Write a JavaScript function to check if a given number is prime or not.
+
+const isPrime = (num) => {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};

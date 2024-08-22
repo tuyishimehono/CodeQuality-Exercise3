e794a5ad549a6f1daed7df9227699bf7cf366f77
tuyishimehono <tuhonori1@gmail.com>
2024-08-22 17:16:46 +0200

    Refactor removeDuplicates

--- Exercise4.js
+++ Exercise4.js
@@ -1,5 +1,5 @@
 //Implement a JavaScript function to remove duplicate elements from an array.
 
 const removeDuplicates = (arr) => {
-  return arr.filter((el) => arr.indexOf(el) === arr.lastIndexOf(el));
+  return [...new Set(arr)];
 };

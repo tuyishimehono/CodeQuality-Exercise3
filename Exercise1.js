3b3b87a5186813c578c3de0316e1dfdcc7abc778
tuyishimehono <tuhonori1@gmail.com>
2024-08-22 19:59:36 +0200

    Add getNestingLevel function

--- Exercise1.js
+++ Exercise1.js
@@ -1,1 +1,14 @@
 //Write a function(s) that checks the level of nesting of an array. Then, use that function to flatten the array into a single-dimensional array.
+
+const getNestingLevel = (array) => {
+  let maxNest = 1;
+  array.forEach((e) => {
+    if (Array.isArray(e)) {
+      const depth = getNestingLevel(e);
+      if (depth + 1 > maxNest) {
+        maxNest = depth + 1;
+      }
+    }
+  });
+  return array.flat(maxNest);
+};
